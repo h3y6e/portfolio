ec5ba146c05548c134f342143aadc8740d347ef0
5ebec <hash.a5ebec@gmail.com>
2020-03-02 12:35:19 +0900

    move files

--- config/index.js
+++ config/index.js
@@ -1,9 +1,8 @@
 module.exports = {
-  pathPrefix: `/c`, // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-
+  pathPrefix: `a5e_be_c`,
   siteTitle: `a5e.be/c`, // Navigation and Site Title
   siteTitleShort: `a5e_be_c`, // short_name for manifest
-  siteUrl: `https://a5e.be`, // Domain of your site. No trailing slash!
+  siteUrl: `https://a5e.be/c`, // Domain of your site. No trailing slash!
   siteLanguage: `ja`, // Language Tag on <html> element
   siteLogo: `/logos/logo.png`, // Used for SEO and manifest
   siteDescription: `5ebec's portfolio`,
@@ -15,6 +14,6 @@ module.exports = {
   googleAnalyticsID: `UA-XXXXXX-X`,
 
   // Manifest and Progress color
-  themeColor: `#db7436`,
-  backgroundColor: `#3b3c4f`,
+  themeColor: `#a5ebec`,
+  backgroundColor: `#14202a`,
 }
