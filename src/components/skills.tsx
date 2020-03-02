24854c04560dd98641d7a09de6ed168e467da9e6
5ebec <hash.a5ebec@gmail.com>
2020-03-03 06:29:32 +0900

    modify skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,16 +1,41 @@
-import React from "react"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
+interface SkillsData {
+  skills: {
+    nodes: {
+      langs: string[];
+    }[];
+  };
+}
+
+const query = graphql`
+  query skillsQuery {
+    skills: allSkillsYaml {
+      nodes {
+        langs
+      }
+    }
+  }
+`;
 const Footer: React.FC = () => {
+  const data: SkillsData = useStaticQuery(query);
   return (
     <section id="skills" className="section">
       <h1 data-sal="zoom-in" className="title has-text-centered">
         Skills
       </h1>
       <div className="tags is-centered">
-      <span className="tag">Japanese</span>
+        {data.skills.nodes.map(item => (
+          <>
+            {item.langs.map(lang => (
+              <span className="tag">{lang}</span>
+            ))}
+          </>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
