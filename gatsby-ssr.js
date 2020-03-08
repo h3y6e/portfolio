3f0e5c86049c0f6507f84f80d4a59d6319c84e5a
5ebec <hash.a5ebec@gmail.com>
2020-03-09 03:34:34 +0900

    adapt to PWA

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+const React = require("react");
+
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <noscript key="noscript">
+      Your browser does not support JavaScript.
+    </noscript>
+  ]);
+};
