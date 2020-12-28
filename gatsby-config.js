63189ab13f4603ebef752c63a1ed866c4e57e6d1
5ebec <hash.a5ebec@gmail.com>
2020-12-29 03:24:51 +0900

    apply

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
-        devMode: true,
-      },
+        devMode: true
+      }
     }
   ]
 };
