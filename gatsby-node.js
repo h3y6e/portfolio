28611c3ebc5476c9af7132a3d0b4a5776fa3a4c8
h3y6e <h3y6e@pm.me>
2023-02-07 17:48:15 +0900

    update webfinger

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createPages = async ({ actions }) => {
-  const { createRedirect } = actions;
-
-  createRedirect({
-    fromPath: `/.well-known/webfinger?resource=acct:heyhoe@h3y6e.com`,
-    toPath: `https://fedibird.com/.well-known/webfinger?resource=acct:h3y6e@fedibird.com`,
-    statusCode: 301
-  });
-};
